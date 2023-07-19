8450237ef2e483054f3fc0a98625466733cfd143
Weston Clark <westoclark@gmail.com>
2023-07-19 11:38:11 -0700

    Initial Commit

---
+++ iPhone Studio Mic/iPhone_Studio_MicApp.swift
@@ -0,0 +1,17 @@
+//
+//  iPhone_Studio_MicApp.swift
+//  iPhone Studio Mic
+//
+//  Created by Weston Clark on 7/19/23.
+//
+
+import SwiftUI
+
+@main
+struct iPhone_Studio_MicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
