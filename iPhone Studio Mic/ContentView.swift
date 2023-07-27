20ff28f905aceb836d17dac25e74fdb3fb02f45f
Weston Clark <westoclark@gmail.com>
2023-07-26 20:10:53 -0700

    clean up audio callbacks, remove input node tap

--- iPhone Studio Mic/ContentView.swift
+++ iPhone Studio Mic/ContentView.swift
@@ -17,9 +17,10 @@ struct ContentView_Previews: PreviewProvider {
 }
 
 struct AudioRoutingView: View {
-    @ObservedObject private var audioHandler = AudioHandler()
     @State private var audioEngineRunning = false
-
+    
+    private let engine = AVAudioEngine()
+    
     var body: some View {
         VStack {
             Image(systemName: "mic") // Google microphone icon
@@ -27,14 +28,13 @@ struct AudioRoutingView: View {
                 .scaledToFit()
                 .frame(width: 60)
                 .padding(.bottom, 60)
-
+            
             Button(action: {
                 if audioEngineRunning {
-                    stopAudioEngine(engine: audioHandler.engine)
+                    stopAudio()
                     audioEngineRunning = false
                 } else {
-                    audioHandler.setupAudioSession()
-                    startAudioEngine(engine: audioHandler.engine)
+                    startAudio()
                     audioEngineRunning = true
                 }
             }) {
@@ -47,37 +47,24 @@ struct AudioRoutingView: View {
             .cornerRadius(10)
         }
     }
-
-    private func startAudioEngine(engine: AVAudioEngine) {
-            engine.inputNode.installTap(onBus: 0, bufferSize: 0, format: engine.inputNode.outputFormat(forBus: 0)) { (buffer, _) in }
-            engine.connect(engine.inputNode, to: engine.outputNode, format: engine.inputNode.outputFormat(forBus: 0))
-
-            do {
-                try engine.start()
-            } catch {
-                fatalError("Failed to start the audio engine: \(error)")
-            }
-        }
-
-    private func stopAudioEngine(engine: AVAudioEngine) {
-            engine.stop()
-            engine.inputNode.removeTap(onBus: 0)
-        }
-    }
-
-class AudioHandler: ObservableObject {
-    let engine = AVAudioEngine()
-
-    func setupAudioSession() {
-        let audioSession = AVAudioSession.sharedInstance()
+    
+    func startAudio() {
+        let session = AVAudioSession.sharedInstance()
+        
+        engine.connect(engine.inputNode, to: engine.outputNode, format: engine.inputNode.inputFormat(forBus: 0))
+        
         do {
-            try audioSession.setCategory(.playAndRecord)
-//            try audioSession.setMode(.measurement)
-            try audioSession.setPreferredIOBufferDuration(0.00005)
-            try audioSession.setActive(true)
-//            print(audioSession.ioBufferDuration)
-        } catch {
-            fatalError("Failed to configure audio session: \(error)")
-        }
+            try session.setCategory(.playAndRecord)
+            try session.setPreferredIOBufferDuration(0.0005)
+//            try session.setMode(.measurement)
+            try engine.start()
+        } catch { fatalError("Can't start audio engine") }
     }
+    
+    func stopAudio() {
+        engine.stop()
+    }
+    
 }
+
+
